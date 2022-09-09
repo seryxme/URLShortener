17e5a029c39b7a19527e53c7d20982aa155380b5
seryxme <coolbams24@yahoo.com>
2022-09-09 22:59:52 +0100

    URL app updates

--- src/main/java/com/myurl/shortener/controllers/URLController.java
+++ src/main/java/com/myurl/shortener/controllers/URLController.java
@@ -1,11 +1,13 @@
 package com.myurl.shortener.controllers;
 
+import com.myurl.shortener.dtos.requests.GetLongURLRequest;
 import com.myurl.shortener.dtos.requests.GetShortURLRequest;
 import com.myurl.shortener.exceptions.InvalidURLExecption;
 import com.myurl.shortener.services.URLService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,15 @@ public class URLController {
     public ResponseEntity<?> getShortURL(@RequestBody GetShortURLRequest getURLRequest) {
         try {
             return new ResponseEntity<>(urlService.getShortURL(getURLRequest), HttpStatus.CREATED);
+        } catch (InvalidURLExecption ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getLongURL(@RequestBody GetLongURLRequest getURLRequest) {
+        try {
+            return new ResponseEntity<>(urlService.getLongURL(getURLRequest), HttpStatus.CREATED);
         } catch (InvalidURLExecption ex) {
             return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
         }
